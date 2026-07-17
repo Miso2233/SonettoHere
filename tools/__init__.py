d4df7ad2da06d7bf6988afb6aa4a0b7116c1097f
Miso2233 <1015789185@qq.com>
2026-07-17 21:27:38 +0800

    refactor(tools): 重命名 verify_image 为 read_image，文件名对齐工具名

--- tools/__init__.py
+++ tools/__init__.py
@@ -48,7 +48,7 @@ def get_all_tools() -> list[BaseTool]:
     from tools.network.tool_weather import WeatherTool
     from tools.network.tool_holiday import HolidayCalendarTool
     from tools.network.tool_image_understand import ImageUnderstandTool
-    from tools.network.tool_verify_image import ReadImageTool
+    from tools.network.tool_read_image import ReadImageTool
     from tools.network.tavily import TavilySearchTool, TavilyExtractTool
 
     # Files
